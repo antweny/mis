06c2a7a3b06f86b988abf31ccb05b22442ab81d5
antweny <antweny@live.com>
2021-04-01 17:12:29 +0300

    system updates

--- resources/views/vouchers/_form.blade.php
+++ resources/views/vouchers/_form.blade.php
@@ -1,25 +1,27 @@
 <div class="form-group">
-    <x-form.elements.label name="Payment: <span class='star'>*</span>" for="payment" />
+    <x-form.label name="Payment: <span class='star'>*</span>" for="payment" />
     <x-dropdown.payment :model="$voucher" />
 </div>
 
 <div class="form-group">
-    <x-form.elements.label name="Descriptions: <span class='star'>*</span>" />
+    <x-form.label name="Descriptions: <span class='star'>*</span>" />
     <textarea name="desc" class="form-control @error('desc') is-invalid @enderror" id="desc" rows="4" required>{{old('desc',$voucher->desc)}}</textarea>
+    @error('desc') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
 </div>
 
 <div class="form-group">
-    <x-form.elements.label name="Particulars" />
+    <x-form.label name="Particulars" />
     <textarea name="particulars" class="form-control @error('particulars') is-invalid @enderror" id="particulars" rows="4" >{{old('particulars',$voucher->particulars)}}</textarea>
+    @error('particulars') <span class="invalid-feedback" role="alert"> <strong>{{ $message }}</strong> </span> @enderror
 </div>
-
+    
 <div class="form-group row">
     <div class="col">
         <div class="float-left">
-            <x-button.back> {{route('vouchers.index')}} </x-button.back>
+            <x-button.back />
         </div>
         <div class="float-right">
-            <x-button label="{{$buttonText}}"/>
+            <x-button.submit label="{{$buttonText}}"/>
         </div>
     </div>
 </div>
